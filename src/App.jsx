949b67f0046fda8be5d1dbd61937fea60ab306d4
Manish <manishshekhawat756@gmail.com>
2025-06-02 00:49:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const customDictionary = {
+  teh: "the",
+
+  wrok: "work",
+
+  fot: "for",
+
+  exampl: "example",
+};
+
+function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  useEffect(() => {
+    const inputArray = input.split(" ");
+
+    const misspelledWord = inputArray.find((word) => customDictionary[word]);
+
+    if (misspelledWord) {
+      setOutput(customDictionary[misspelledWord]);
+    } else {
+      setOutput(""); // clear output if nothing found
+    }
+  }, [input]);
+
+  return (
+    <>
+      <h1>Spell Check and Auto-Correction</h1>
+      <textarea
+        placeholder="Enter text..."
+        onChange={handleChange}
+        style={{ width: "300px", height: "100px" }}
+      />
+      {output && <p>Did you mean: {output}</p>}
+    </>
+  );
+}
+
+export default App;
